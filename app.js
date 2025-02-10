5747c1b2f1eda7e3bb3acdd1b65a7b02ffc737e4
Andrew Smith <meuzishun@gmail.com>
2025-02-10 07:52:37 -0500

    Create app.js file

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const { corsOptions } = require('./config/corsOptions');
+const compression = require('compression');
+const helmet = require('helmet');
+const { limiter } = require('./config/limiter');
+const { routes } = require('./routes/index');
+const { errorHandler } = require('./middleware/errorMiddleware');
+
+const app = express();
+app.options('*', cors(corsOptions));
+app.set('trust proxy', 1);
+app.use(cors(corsOptions));
+app.use(limiter);
+app.use(compression());
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use('/api', routes);
+app.use(errorHandler);
+
+module.exports = { app };
