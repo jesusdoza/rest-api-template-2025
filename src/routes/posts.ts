2bc083c731ecb89cbb0b1294ddfc179997492f87
jesus doza <jesus.doza@gmail.com>
2025-07-12 11:24:31 -0500

    fix tsc src/routes/posts.ts

--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -1,36 +1,36 @@
-const router = require('express').Router();
+const router = require("express").Router();
 const {
   titleValidator,
   contentValidator,
   postIdValidator,
   checkValidations,
-} = require('../middleware/inputValidators');
+} = require("../middleware/inputValidators");
 const {
   getPosts,
   getSinglePost,
   createPost,
   editPost,
   deletePost,
-} = require('../controllers/postsController');
-const { isAdmin } = require('../middleware/authMiddleware');
+} = require("../controllers/postsController");
+import { isAdmin } from "../middleware/authMiddleware";
 
-router.get('/', getPosts);
-router.get('/:postId', postIdValidator, checkValidations, getSinglePost);
+router.get("/", getPosts);
+router.get("/:postId", postIdValidator, checkValidations, getSinglePost);
 router.post(
-  '/',
+  "/",
   titleValidator,
   contentValidator,
   checkValidations,
   createPost
 );
 router.put(
-  '/:postId',
+  "/:postId",
   isAdmin,
   titleValidator,
   contentValidator,
   checkValidations,
   editPost
 );
-router.delete('/:postId', isAdmin, deletePost);
+router.delete("/:postId", isAdmin, deletePost);
 
 module.exports = router;
