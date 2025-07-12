fa0940e7c3d4ef003849af6412e2f0382c64d9dc
jesus doza <jesus.doza@gmail.com>
2025-07-12 11:12:40 -0500

    fix tsc src/lib/issueJWT.ts

--- src/lib/issueJWT.ts
+++ src/lib/issueJWT.ts
@@ -1,16 +1,16 @@
-require('dotenv').config();
-const fs = require('fs');
-const path = require('path');
-const jsonwebtoken = require('jsonwebtoken');
+require("dotenv").config();
+import fs from "fs";
+// const path = require('path');
+import jsonwebtoken from "jsonwebtoken";
 
 const rootDir = process.cwd();
-const pathToPrivKey = path.join(rootDir, 'id_rsa_priv.pem');
-const PRIV_KEY = fs.readFileSync(pathToPrivKey, 'utf8');
+const pathToPrivKey = path.join(rootDir, "id_rsa_priv.pem");
+const PRIV_KEY = fs.readFileSync(pathToPrivKey, "utf8");
 
-function issueJWT(user) {
+function issueJWT(user: { _id: string }) {
   const _id = user._id;
 
-  const expiresIn = '2h';
+  const expiresIn = "2h";
 
   const payload = {
     sub: _id,
@@ -18,7 +18,7 @@ function issueJWT(user) {
 
   const signedToken = jsonwebtoken.sign(payload, PRIV_KEY, {
     expiresIn: expiresIn,
-    algorithm: 'RS256',
+    algorithm: "RS256",
   });
 
   return {
